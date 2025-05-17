ae60dde896126bfb412c11d607dbcf2f85081e86
DoriaFabio <doriafabio2001@gmail.com>
2025-05-17 20:38:48 +0200

    do snack 1 bonus

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@
 let dato: unknown;
 switch (typeof dato) {
     case "string":
-        dato = dato.toUpperCase();
+        console.log(dato.toUpperCase());
         break;
     case "number":
-        dato = dato * 2;
+        console.log(dato * 2);
         break;
     case "boolean":
         if (dato === true) {
@@ -15,18 +15,13 @@ switch (typeof dato) {
         }
         break;
     default:
-        console.log("Tipo non supportato");
+        if (dato === null) {
+            console.log("Il dato è vuoto");
+        } else if (Array.isArray(dato)) {
+            console.log(dato.length);
+        } else if (dato instanceof Promise) {
+            dato.then((msg) => console.log(msg))
+        } else {
+            console.log("Tipo non supportato");
+        }
 }
-
-
-//! 🏆 Snack 1
-//* Hai ricevuto un dato generico da un'API, ma non sai di che tipo sia…
-//* Il tuo compito è controllare il tipo del dato e stampare il valore in modo corretto.
-//todo     Se è una stringa: stampala in maiuscolo
-//todo     Se è un numero: moltiplicalo per due e stampalo
-//todo    Se è un booleano: stampa “Sì” o “No” in base al suo valore
-//todo     In tutti gli altri casi: stampa “Tipo non supportato”
-//! 🎯 BONUS
-//?     Se è null: stampa “Il dato è vuoto”
-//?     Se è un array: stampa la sua lunghezza
-//?     Se è una Promise: attendi che si risolva e stampa il valore del resolve.
